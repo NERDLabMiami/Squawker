1baf7c4f69a9798f0883e65788f5e1417f22fa29
Clay <c.ewing@miami.edu>
2016-02-22 11:47:04 -0500

    qualtrics update

--- Assets/Scripts/Helpers/Career.cs
+++ Assets/Scripts/Helpers/Career.cs
@@ -5,9 +5,15 @@ using System.Collections;
 public class Career : MonoBehaviour {
 	public Player player;
 	public GameObject avatarCreator;
+	public GameObject continueButton;
 	// Use this for initialization
 
 	void Start () {
+		int started = PlayerPrefs.GetInt("game in progress", 0);
+		if (started == 1) {
+			continueButton.SetActive(true);
+		}
+
 	}
 	
 	public void startMonth() {

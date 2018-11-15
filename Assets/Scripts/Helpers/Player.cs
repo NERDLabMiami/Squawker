1df527463cb39ae48b00366d6bf83fe4131ddcec
Clay Ewing <c.ewing@miami.edu>
2018-11-15 11:00:48 -0500

    Update Unity version
    fix static remap bug by removing method. might be unused.

--- Assets/Scripts/Helpers/Player.cs
+++ Assets/Scripts/Helpers/Player.cs
@@ -597,9 +597,10 @@ public class Player : MonoBehaviour {
 		loadSceneNumber(1);
 	}
 
-
+    /*
 	public float Remap (this float value, float from1, float to1, float from2, float to2) {
 		return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
 	}
+    */
 		
 }
